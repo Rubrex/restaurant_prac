858a88f41bb883568f9c35be8c00a7f3d15796bb
Rubel <rubrex96@hotmail.com>
2022-02-06 23:38:44 +0600

    Navigation bar using reactstrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Navigation from "./Navigation";
 const Header = () => {
-  return <div>Hello from Header</div>;
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 };
 
 export default Header;
