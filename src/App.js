9d45dd922c136fdb8fe3d29955d16c3ce2dd0fe0
Rubel <rubrex96@hotmail.com>
2022-02-06 23:30:46 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import MainComponent from "./components/MainComponents";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
 }
 
 export default App;
